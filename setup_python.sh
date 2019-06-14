9dc1f765d1ec244f91dd3dfe7113d64fb1602c4d
MarcusJones <jones.0bj3@gmail.com>
2019-06-14 14:09:33 +0200

    Create setup_python.sh

---
+++ setup_python.sh
@@ -0,0 +1,10 @@
+sudo apt update
+
+# Install python3, and link to the python command
+sudo apt install python3
+sudo ln /usr/bin/python3 /usr/bin/python
+
+#Install pip3 and link
+sudo apt install -y python3-pip
+sudo ln /usr/bin/pip3 /usr/bin/pip
+
