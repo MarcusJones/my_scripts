21674c9928aa45a60f832ad780800821b166bc53
MarcusJones <jones.0bj3@gmail.com>
2019-06-14 14:12:30 +0200

    Update setup_python.sh

--- setup_python.sh
+++ setup_python.sh
@@ -1,10 +1,16 @@
 sudo apt update
 
 # Install python3, and link to the python command
-sudo apt install python3
+sudo apt install -y python3
 sudo ln /usr/bin/python3 /usr/bin/python
 
 #Install pip3 and link
 sudo apt install -y python3-pip
 sudo ln /usr/bin/pip3 /usr/bin/pip
 
+#Install pyenv to manage python versions
+curl https://pyenv.run | bash
+# Add to .bashrc:
+echo 'export PATH="/home/ubuntu/.pyenv/bin:$PATH"' >>~/.bashrc
+echo 'eval "$(pyenv init -)"' >>~/.bashrc
+echo 'eval "$(pyenv virtualenv-init -)"' >>~/.bashrc
